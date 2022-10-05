2875ed9652e928a8b82bf84a19fcba1edd42420a
desaiHarsh183 <desaiharsh183@gmail.com>
2022-10-05 16:02:37 +0530

    project done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,27 +15,31 @@ export default function Contact() {
     </div>
     <div className="lg:w-1/2 md:w-2/3 mx-auto">
       <div className="flex flex-wrap -m-2">
-        <div className="p-2 w-1/2">
-          <div className="relative">
-            <label for="name" className="leading-7 text-sm text-gray-600">Name</label>
-            <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-pink-500 focus:bg-white focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+      <form action='https://script.google.com/macros/s/AKfycbyvlddrE6PRsBXDJE3OgC82OOJTHFTyra6Fg3e__wKNg6WTxV71b-yH8Viztu-8vylmYw/exec' method='POST' className="flex flex-wrap -m-2">
+          <div className="p-2 w-1/2">
+            
+              <div className="relative">
+                <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
+                <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-pink-500 focus:bg-white focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required />
+              </div>
           </div>
-        </div>
-        <div className="p-2 w-1/2">
-          <div className="relative">
-            <label for="email" className="leading-7 text-sm text-gray-600">Email</label>
-            <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-pink-500 focus:bg-white focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+          <div className="p-2 w-1/2">
+            <div className="relative">
+              <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
+              <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-pink-500 focus:bg-white focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" required />
+            </div>
           </div>
-        </div>
-        <div className="p-2 w-full">
-          <div className="relative">
-            <label for="message" className="leading-7 text-sm text-gray-600">Message</label>
-            <textarea id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-pink-500 focus:bg-white focus:ring-2 focus:ring-pink-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+      
+          <div className="p-2 w-full">
+            <div className="relative">
+              <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
+              <textarea id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-pink-500 focus:bg-white focus:ring-2 focus:ring-pink-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" required></textarea>
+            </div>
+           </div>
+          <div className="p-2 w-full">
+            <button className="flex mx-auto text-white bg-pink-500 border-0 py-2 px-8 focus:outline-none hover:bg-pink-600 rounded text-lg">Submit</button>
           </div>
-        </div>
-        <div className="p-2 w-full">
-          <button className="flex mx-auto text-white bg-pink-500 border-0 py-2 px-8 focus:outline-none hover:bg-pink-600 rounded text-lg">Button</button>
-        </div>
+        </form>
         <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
           <span className="text-pink-500">sanskrutisakharkar678@gmail.com</span>
           <p className="leading-normal my-5">49 Smith St.
